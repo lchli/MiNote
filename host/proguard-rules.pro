ac3244a092e1df12fafa4a01a01dcaefe50554fa
lichenghang <lichenghang@babytree-inc.com>
2018-06-12 19:46:40 +0800

    proguard

--- host/proguard-rules.pro
+++ host/proguard-rules.pro
@@ -23,3 +23,66 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+# Add project specific ProGuard rules here.
+# By default, the flags in this file are appended to flags specified
+# in D:\android\sdk/tools/proguard/proguard-android.txt
+# You can edit the include path and order by changing the proguardFiles
+# directive in build.gradle.
+#
+# For more details, see
+#   http://developer.android.com/guide/developing/tools/proguard.html
+
+# Add any project specific keep options here:
+
+# If your project uses WebView with JS, uncomment the following
+# and specify the fully qualified class name to the JavaScript interface
+# class:
+#-keepclassmembers class fqcn.of.javascript.interface.for.webview {
+#   public *;
+#}
+#okhttp
+-dontwarn com.squareup.okhttp.**
+-keep class com.squareup.okhttp.** { *;}
+-dontwarn okio.**
+-dontwarn javax.**
+
+#entity.
+-keep class * implements java.io.Serializable
+-keepclassmembers class * implements java.io.Serializable { *; }
+
+
+#gson.
+-dontnote com.google.gson.**
+-dontwarn com.google.gson.**
+-keep class com.google.gson.** { *; }
+
+
+#fastjson
+-dontwarn com.alibaba.fastjson.**
+-keep class com.alibaba.fastjson.**{*; }
+
+#定位
+-keep class com.amap.api.location.**{*;}
+-keep class com.loc.**{*;}
+-keep class com.amap.api.fence.**{*;}
+-keep class com.autonavi.aps.amapapi.model.**{*;}
+#retrofit
+# Platform calls Class.forName on types which do not exist on Android to determine platform.
+-dontnote retrofit2.Platform
+# Platform used when running on Java 8 VMs. Will not be used at runtime.
+-dontwarn retrofit2.Platform$Java8
+# Retain generic type information for use by reflection by converters and adapters.
+-keepattributes Signature
+# Retain declared checked exceptions for use by a Proxy instance.
+-keepattributes Exceptions
+
+### greenDAO 3
+-keepclassmembers class * extends org.greenrobot.greendao.AbstractDao {
+public static java.lang.String TABLENAME;
+}
+-keep class **$Properties
+
+# If you do not use SQLCipher:
+-dontwarn org.greenrobot.greendao.database.**
+# If you do not use RxJava:
+-dontwarn rx.**
---
+++ host/src/main/java/com/lch/menote/note/data/db/ReleaseDbOpenHelper.java
@@ -0,0 +1,81 @@
+package com.lch.menote.note.data.db;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.apkfuns.logutils.LogUtils;
+import com.lch.menote.ConstantUtil;
+import com.lch.menote.note.data.db.gen.DaoMaster;
+import com.lch.netkit.common.tool.ExtFileUtils;
+
+import org.apache.commons.io.FileUtils;
+import org.greenrobot.greendao.database.Database;
+
+import java.io.File;
+
+public class ReleaseDbOpenHelper extends DaoMaster.DevOpenHelper {
+
+
+    public ReleaseDbOpenHelper(Context context, String name, boolean isSdcardDatabase) {
+        super(chooseContext(context, isSdcardDatabase), name);
+    }
+
+    public ReleaseDbOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, boolean isSdcardDatabase) {
+        super(chooseContext(context, isSdcardDatabase), name, factory);
+    }
+
+    private static Context chooseContext(Context context, boolean isSdcardDatabase) {
+        if (!isSdcardDatabase) {
+            return context;
+        }
+
+        return new ContextWrapper(context) {
+
+            @Override
+            public File getDatabasePath(String name) {
+                try {
+                    FileUtils.forceMkdir(new File(ConstantUtil.DB_DIR));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                File noteDb = new File(ConstantUtil.DB_DIR, name);
+
+                LogUtils.e("DB_DIR exists:" + new File(ConstantUtil.DB_DIR).exists());
+
+                ExtFileUtils.makeFile(noteDb.getAbsolutePath());
+
+                LogUtils.e("db:" + noteDb.getAbsolutePath());
+
+                return noteDb;
+            }
+
+            @Override
+            public SQLiteDatabase openOrCreateDatabase(String name, int mode, SQLiteDatabase.CursorFactory factory) {
+                return SQLiteDatabase.openOrCreateDatabase(getDatabasePath(name), factory);
+            }
+
+            @Override
+            public SQLiteDatabase openOrCreateDatabase(String name, int mode, SQLiteDatabase.CursorFactory factory, DatabaseErrorHandler errorHandler) {
+                return SQLiteDatabase.openOrCreateDatabase(getDatabasePath(name).getPath(), factory, errorHandler);
+            }
+        };
+    }
+
+
+    @Override
+    public void onUpgrade(Database db, int oldVersion, int newVersion) {
+        Log.e("greenDAO", "Upgrading schema from version $oldVersion to $newVersion by dropping all tables");
+
+        if (oldVersion == 1) {
+            db.execSQL("ALTER TABLE note ADD CATEGORY integer");
+        } else {
+            DaoMaster.dropAllTables(db, true);
+            onCreate(db);
+        }
+
+    }
+}
